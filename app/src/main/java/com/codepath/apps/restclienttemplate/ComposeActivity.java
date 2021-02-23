d7f7320b106b32f6f1854f5db75285d88940fda0
juhichaudhari <juhichaudhari@gmail.com>
2021-02-22 20:49:49 -0500

    While composing a tweet, user can see a character counter with characters remaining for tweet out of 280

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,11 +3,15 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -24,6 +28,7 @@ public class ComposeActivity extends AppCompatActivity {
     public static final int MAX_TWEET_LIMIT = 140;
     EditText etCompose;
     Button btnTweet;
+    TextView etValue;
 
     TwitterClient client;
 
@@ -36,6 +41,37 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        etValue = findViewById(R.id.etValue);
+
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int chars = s.toString().length();
+                String temp = String.valueOf(chars);
+                // Fires right after the text has changed
+                etValue.setText(temp);
+
+                if (chars > 280)
+                {
+                    etValue.setTextColor(Color.RED);
+                }
+                else{
+                    etValue.setTextColor(Color.BLACK);
+                }
+            }
+        });
 
         // set click listner on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
